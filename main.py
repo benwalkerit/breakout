087e5ea8fbafb9ef1ef0497ad58df35bbf740b85
benwalkerit <benrwalker@icloud.com>
2023-07-27 15:45:36 +0100

    continuing to troubleshoot

--- main.py
+++ main.py
@@ -36,13 +36,22 @@ def pause_game():
 
 #Check to see whether the ball collides with the wall
 def check_collision_with_walls():
+
     global ball, score, playing_game, ui
-    if ball.xcor() < -500 or ball.xcor() > 570:
+
+    # detect collision with left and right walls:
+    if ball.xcor() < -580 or ball.xcor() > 570:
         ball.bounce(x_bounce=True, y_bounce=False)
         return
+
+    # detect collision with upper wall
     if ball.ycor() > 270:
-        ball.bounce(x_bounce=False, y_bounce = True)
+        ball.bounce(x_bounce=False, y_bounce=True)
         return
+
+    # detect collision with bottom wall
+    # In this case, user failed to hit the ball
+    # thus he loses. The game resets.
     if ball.ycor() < -280:
         ball.reset()
         score.decrease_lives()
@@ -56,50 +65,78 @@ def check_collision_with_walls():
 
 #Check to see whether the ball collides with the paddle
 def check_collision_with_paddle():
-    global ball, paddle 
+
+    global ball, paddle
+    # record x-axis coordinates of ball and paddle
     paddle_x = paddle.xcor()
     ball_x = ball.xcor()
+
+    # check if ball's distance(from its middle)
+    # from paddle(from its middle) is less than
+    # width of paddle and ball is below a certain
+    #coordinate to detect their collision
     if ball.distance(paddle) < 110 and ball.ycor() < -250:
+
+        # If Paddle is on Right of Screen
         if paddle_x > 0:
             if ball_x > paddle_x:
+                # If ball hits paddles left side it
+                # should go back to left
                 ball.bounce(x_bounce=True, y_bounce=True)
                 return
             else:
                 ball.bounce(x_bounce=False, y_bounce=True)
                 return
+
+        # If Paddle is left of Screen
         elif paddle_x < 0:
             if ball_x < paddle_x:
+                # If ball hits paddles left side it
+                # should go back to left
                 ball.bounce(x_bounce=True, y_bounce=True)
                 return
             else:
-                ball.bounce(x_bounce=False, y_bounce= True)
+                ball.bounce(x_bounce=False, y_bounce=True)
                 return
+
+        # Else Paddle is in the Middle horizontally
         else:
             if ball_x > paddle_x:
                 ball.bounce(x_bounce=True, y_bounce=True)
                 return
             elif ball_x < paddle_x:
-                    ball.bounce(x_bounce=True, y_bounce=True)
-                    return
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return
             else:
                 ball.bounce(x_bounce=False, y_bounce=True)
+                return
 
 #Check to see whether the ball collides with the bricks
 def check_collision_with_bricks():
-    global ball, bricks
+    global ball, score, bricks
+
     for brick in bricks.bricks:
         if ball.distance(brick) < 40:
+            score.increase_score()
             brick.quantity -= 1
             if brick.quantity == 0:
                 brick.clear()
                 brick.goto(3000, 3000)
                 bricks.bricks.remove(brick)
+
+            # detect collision from left
             if ball.xcor() < brick.left_wall:
                 ball.bounce(x_bounce=True, y_bounce=False)
+
+            # detect collision from right
             elif ball.xcor() > brick.right_wall:
                 ball.bounce(x_bounce=True, y_bounce=False)
+
+            # detect collision from bottom
             elif ball.ycor() < brick.bottom_wall:
                 ball.bounce(x_bounce=False, y_bounce=True)
+
+            # detect collision from top
             elif ball.ycor() > brick.upper_wall:
                 ball.bounce(x_bounce=False, y_bounce=True)      
 
@@ -111,7 +148,7 @@ screen.onkey(pause_game, "space")
 
 
 while playing_game:
-    if game_paused:
+    if not game_paused:
         screen.update()
         time.sleep(0.01)
         ball.move()
